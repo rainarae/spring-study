ca4c60dede29ef01b85a489979e54265419db2b0
Narae Lee <nr1131@gmail.com>
2018-02-01 20:29:09 +0900

    project init

---
+++ SpringMaster/src/main/java/com/study/springmaster/SpringMasterApplication.java
@@ -0,0 +1,12 @@
+package com.study.springmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMasterApplication.class, args);
+	}
+}
